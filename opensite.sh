71265638ba68b70de677aa28622a1ba14efc613a
ahmadsholik <ahmadsholik@gmail.com>
2022-02-05 10:12:17 +0700

    Update opensite.sh

--- opensite.sh
+++ opensite.sh
@@ -3,8 +3,8 @@
 #    echo "$field1 and $field2"
 #    
 #done < "link.txt mail.txt"
-input1=https://raw.githubusercontent.com/ahmadsholik/linkYT/main/link.txt
-input2=https://raw.githubusercontent.com/ahmadsholik/linkYT/main/mail.txt
+for input1 $(cat https://raw.githubusercontent.com/ahmadsholik/linkYT/main/link.txt); do echo $input1; done
+for input2 $(cat https://raw.githubusercontent.com/ahmadsholik/linkYT/main/mail.txt); do echo $input1; done
 while read -u 3 -r line1 && read -u 4 -r line2; do
   # process the lines
   # note that the loop will end when we reach EOF on either of the files, because of the `&&`
