28ac449aca1a9734e68179ba300a77ac33344ba7
ahmadsholik <ahmadsholik@gmail.com>
2022-02-05 09:52:07 +0700

    Update opensite.sh

--- opensite.sh
+++ opensite.sh
@@ -1,5 +1,11 @@
-while IFS=, read -r field1 field2
-do
-    echo "$field1 and $field2"
-    
-done < "link.txt mail.txt"
+#while IFS=, read -r field1 field2
+#do
+#    echo "$field1 and $field2"
+#    
+#done < "link.txt mail.txt"
+
+while read -u 3 -r line1 && read -u 4 -r line2; do
+  # process the lines
+  # note that the loop will end when we reach EOF on either of the files, because of the `&&`
+ chromium-browser $line1 --profile-mail=$line2
+done 3< link.txt 4< mail.txt
