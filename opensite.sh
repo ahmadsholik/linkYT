c59604949b7dfc10ea9175f8108d38d33dde02b7
ahmadsholik <ahmadsholik@gmail.com>
2022-02-05 09:59:33 +0700

    Update opensite.sh

--- opensite.sh
+++ opensite.sh
@@ -8,4 +8,4 @@ while read -u 3 -r line1 && read -u 4 -r line2; do
   # process the lines
   # note that the loop will end when we reach EOF on either of the files, because of the `&&`
  chromium-browser $line1 --profile-mail=$line2
-done 3< link.txt 4< mail.txt
+done 3< https://raw.githubusercontent.com/ahmadsholik/linkYT/main/link.txt 4< https://raw.githubusercontent.com/ahmadsholik/linkYT/main/mail.txt
