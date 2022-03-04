bdc9a3b734aff5b03307a815c15ae4575251a90a
ahmadsholik <ahmadsholik@gmail.com>
2022-03-04 09:28:22 +0700

    Create openyt.sh

---
+++ openyt.sh
@@ -0,0 +1,9 @@
+xdg-open https://youtu.be/PznEwgw-TQ0 --profile-email=luqman.alkgm@gmail.com
+sleep 1m
+pkill -o -i "Chromium-browser"
+xdg-open https://youtu.be/PznEwgw-TQ0 --profile-email=luqman.alkgm@gmail.com
+sleep 1m
+pkill -o -i "Chromium-browser"
+xdg-open https://youtu.be/PznEwgw-TQ0 --profile-email=luqman.alkgm@gmail.com
+sleep 1m
+pkill -o -i "Chromium-browser"
